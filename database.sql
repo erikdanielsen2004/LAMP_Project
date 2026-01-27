5086597cd3454b57e32d8e12741c4420671380c3
Corday Davis <164126689+cdavis190@users.noreply.github.com>
2026-01-27 15:44:29 -0500

    to not doxx the users and passwords

--- database.sql
+++ database.sql
@@ -26,29 +26,3 @@ CREATE TABLE `COP4331`.`Contacts`
 	`DateCreated` DATE NOT NULL , -- Requirement for date record created
 	PRIMARY KEY (`ID`)
 ) ENGINE = InnoDB;
-
-
-insert into Contacts (FirstName, LastName, Phone, Email, UserID, DateCreated) VALUES ('Frankie', 'Ariso', '3214567890', 'frankie123@gmail.com', 21, CURRENT_DATE()); -- Temporary contact
-
-
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Corday','Davis','CordayD','Test1');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Maya','Hernandez','MayaH','TestM');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Ethan','Reed','EthanR','TestE');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Sofia','Nguyen','SofiaN','TestS');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Jordan','Parker','JordanP','TestJ');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Aaliyah','Brooks','AaliyahB','TestA');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Carlos','Vega','CarlosV','TestC');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Priya','Shah','PriyaS','TestP');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Noah','Bennett','NoahB','TestN');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Zoe','Foster','ZoeF','TestZ');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Liam','Carter','LiamC','TestL');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Nina','Khan','NinaK','TestN');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Tyler','Morris','TylerM','TestT');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Olivia','Stone','OliviaS','TestO');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Ben','Ramirez','BenR','TestB');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Leila','Simmons','LeilaS','TestL');
-INSERT INTO Users (FirstName, LastName, Login, Password) VALUES ('Hugo','Diaz','HugoD','TestH');
-
-use COP4331;
-create user 'TheBeast' identified by 'WeLoveCOP4331';
-grant all privileges on COP4331.* to 'TheBeast'@'%'; -- Special database user!
