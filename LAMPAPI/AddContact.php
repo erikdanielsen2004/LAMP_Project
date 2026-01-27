73b4a98d3b632347ca3bf3979bd4130dce9d45a8
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-01-27 15:09:46 -0500

    Add files via upload
    test 1 of addContact endpoint

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,39 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$color = $inData["color"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (ID,FirstName,LastName,Phone,Email,UserID,DateCreated) VALUES(?,?,?,?,?,?,?)");
+		$stmt->bind_param("sssssss", $id, $fName, $lName, $phone, $email, $userId, $dateCreated);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
