003093985b63021b662ce63420aaa5c818fee58d
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-01-29 15:25:37 -0500

    added error for invalid phone (draft 1)

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,41 +1,57 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$color = $inData["color"];
-	$userId = $inData["userId"];
+$inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Phone,Email,UserID,DateCreated) VALUES(?,?,?,?,?,?)");
-		$stmt->bind_param("ssssss", $inData["fName"], $inData["lName"], $inData["phone"], $inData["email"], $inData["userId"], $inData["dateCreated"]);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	} // missing to account for errors: invalid phone, invalid email, etc.
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$phone = $inData["phone"];
+$email = $inData["email"];
+$userId = $inData["userId"];
+$dateCreated = $inData["dateCreated"];
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+	if (validPhone($phone)) {
+
+		returnWithError("Invalid phone number.");
 	}
+	if (validEmail($email)) {
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
+		returnWithError("Invalid email number.");
 	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+	$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Phone,Email,UserID,DateCreated) VALUES(?,?,?,?,?,?)");
+	$stmt->bind_param("ssssss", $firstName, $lastName, $phone, $email, $userId, $dateCreated);
+	$stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("");
+}
 
-?>
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
 
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function returnWithError($err)
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
+
+function validPhone($obj)
+{
+	if (strlen($obj) != 10) return false;
+
+	for ($i = 0; $i < 10; ++$i) if (!is_numeric($obj[$i])) return false;
+
+	return true;
+}
+
+function validEmail($obj) {}
