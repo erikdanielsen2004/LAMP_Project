e89e640aa6e59b2906deee3d2720dedc149016e7
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-02-03 15:04:45 -0500

    search contact api endpoint

---
+++ LAMPAPI/SearchContact.php
@@ -0,0 +1,85 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		
+		$firstName = "%" . $inData["firstNameSearch"] . "%";
+		$lastName = "%" . $inData["lastNameSearch"] . "%";
+
+		if ($lastName == "%%") {
+
+			$stmt = $conn->prepare("select FirstName, LastName from Contacts where FirstName like ? and UserID=?");
+			$stmt->bind_param("ss", $firstName, $inData["userId"]);
+
+		} else if ($firstName == "%%") {
+
+			$stmt = $conn->prepare("select FirstName, LastName from Contacts where LastName like ? and UserID=?");
+			$stmt->bind_param("ss", $lastName, $inData["userId"]);
+
+		} else {
+
+			$stmt = $conn->prepare("select FirstName, LastName from Contacts where FirstName like ? and LastName like ? and UserID=?");
+			$stmt->bind_param("sss", $firstName, $lastName, $inData["userId"]);
+
+		}
+		
+		$stmt->execute();
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["Name"] . '"';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
