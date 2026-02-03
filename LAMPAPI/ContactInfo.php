9c05724cdcfcf0be8e280cff9b09d9e303e693de
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-02-03 15:54:57 -0500

    Add files via upload

---
+++ LAMPAPI/ContactInfo.php
@@ -0,0 +1,54 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("select Phone, Email, DateCreated from Contacts where ID=?");
+			$stmt->bind_param("i", $inData["id"]);
+		
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+        if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['Phone'], $row['Email'], $row['DateCreated'] );
+		}
+		else
+		{
+			returnWithError("Loading Error");
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithInfo( $phone, $email, $dateCreated )
+	{
+		$retValue = '{"phone":' . $phone . ',"email":"' . $email . '","dateCreated":"' . $dateCreated . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+	
+?>
